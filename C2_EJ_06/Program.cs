89242e1b6dc3c58e6fe71dc90aaf86518e6a7293
Rubirpg <rubi.rpg@gmail.com>
2022-07-27 12:40:32 +0200

    Agregar archivos de proyecto.

---
+++ C2_EJ_06/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2_Ej_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca el valor para A: ");
+            int numa = Convert.ToInt32(Console.ReadLine()); //leemos y asignamos a numa el valor de A
+            Console.WriteLine("Introduzca el valor para B: ");
+            int numb = Convert.ToInt32(Console.ReadLine()); //leemos y asignamos a numb el valor de B
+            Console.WriteLine("Introduzca el valor para C: ");
+            int numc = Convert.ToInt32(Console.ReadLine()); //leemos y asignamos a numc el valor de C
+
+
+            int numtemp = numa + numb + numc;
+            
+            //mostramos por pantalla los valores contenidos en las variables
+            Console.WriteLine();
+            Console.WriteLine("El valor de A es: " + numa);
+            Console.WriteLine("El valor de B es: " + numb);
+            Console.WriteLine("El valor de C es: " + numc);
+            Console.WriteLine("La suma de los tres valores es: " + numtemp);
+        }
+    }
+}
